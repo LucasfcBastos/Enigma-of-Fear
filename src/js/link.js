2940ffa95977ddc8441e4d6f5188d6de6e0f34a2
Lucas Engineer <lucas.software.engineering@gmail.com>
2026-01-28 19:54:55 -0300

    I stylized the game's purchase options.

---
+++ src/js/link.js
@@ -0,0 +1,13 @@
+// game
+
+function game1() {
+    window.open('https://www.nuuvem.com/br-pt/item/enigma-do-medo', '_blank');
+}
+
+function game2() {
+    window.open('https://www.nuuvem.com/br-pt/item/enigma-do-medo-bundle-do-detetive', '_blank');
+}
+
+function game3() {
+    window.open('https://www.nuuvem.com/br-pt/item/enigma-do-medo-bundle-do-medo', '_blank');
+}
