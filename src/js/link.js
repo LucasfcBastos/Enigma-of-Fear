740422fac4fae768b496b59fe50037e698e315be
Lucas Engineer <lucas.software.engineering@gmail.com>
2026-01-28 21:49:49 -0300

    I made interactions that can be purchased additionally.

--- src/js/link.js
+++ src/js/link.js
@@ -1,4 +1,4 @@
-// game
+// Redirect links to game options.
 
 function game1() {
     window.open('https://www.nuuvem.com/br-pt/item/enigma-do-medo', '_blank');
@@ -10,4 +10,14 @@ function game2() {
 
 function game3() {
     window.open('https://www.nuuvem.com/br-pt/item/enigma-do-medo-bundle-do-medo', '_blank');
+}
+
+// Redirect links to additional purchases.
+
+function excess1() {
+    window.open('https://www.lolja.com.br/ordem-cellbit', '_blank');
+}
+
+function excess2() {
+    window.open('https://jamboeditora.com.br/?post_type=product&s=ordem+paranormal', '_blank');
 }
